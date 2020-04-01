e0e5e46cb9e173f7988fbb25a62b54cf46c2e3f6
RNegroni <62661747+RNegroni@users.noreply.github.com>
2020-04-01 18:32:28 +0200

    Create R_code_spatial.r

---
+++ R_code_spatial.r
@@ -0,0 +1,50 @@
+# R code for spatial view of points
+
+library(sp)
+
+data(meuse)
+head(meuse)
+
+#coordinates as spatial dataset
+coordinates(meuse) = ~x+y
+
+plot(meuse)
+# with sp plot I can select one variable
+spplot(meuse, "zinc")
+
+#Exercise: plot the amount of copper
+
+spplot(meuse, "copper")
+#To change the title
+
+spplot(meuse, "copper", main=copper concentration)
+# instead of colors in the plot we have the size 
+bubble(meuse, "zinc")
+bubble(meuse, "zinc", main="Zinc concentration")
+
+#Exercise
+bubble(meuse, "copper", col="red", main="Copper concentration")
+
+#importing new data(covid_agg.csv into C: in a folder called lab)
+
+#setting the working directory: lab (for windows)
+setwd("C:/lab/")
+
+#import our data, first putting the name, then we link a function to the object
+covid <- read.table("covid_agg.csv", head=TRUE)
+head(covid)
+#let's see the number of cases per country
+
+attach(covid)
+plot(country,cases)
+#per cambiare le x e le y (las=labels)
+plot(country,cases, las=1) #for orizontal labels
+plot(country,cases, las=2) #for perpendicular labels
+plot(country,cases, las=3) #for vertical labels
+
+plot(country,cases, las=3, cex.axis=0.5)
+#let's plot the data, spatially, with ggplot
+
+install.packages("ggplot2")
+library(ggplot2)
+#to save datas in R
