ac15cdb0264e0be765aacd472751581596096eba
RNegroni <62661747+RNegroni@users.noreply.github.com>
2020-04-22 18:03:02 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -2,3 +2,57 @@
 
 install.packages ("raster")
 install.packages ("RStoolbox")
+
+setwd("C:/lab/") 
+library(raster)
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+plot(p224r63_2011)
+
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR
+
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # 
+plot(p224r63_2011, col=cl)
+
+
+par(mfrow=c(2,2))
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+plot(p224r63_2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c('dark green','green','light green'))(100) # 
+plot(p224r63_2011$B2_sre, col=clg)
+
+clr <- colorRampPalette(c('dark red','red','pink'))(100) # 
+plot(p224r63_2011$B3_sre, col=clr)
+
+cln <- colorRampPalette(c('red','orange','yellow'))(100) # 
+plot(p224r63_2011$B4_sre, col=cln)
+
+
+# RGB 
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+# Exercise: mount NIR ontop the G of RGB
+
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+
+# 1988 image
+# p224r63_1988_masked
+
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
