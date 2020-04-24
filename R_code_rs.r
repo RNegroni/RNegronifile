322b466dc5519ab4b7b721b427f62140a181ac6c
RNegroni <62661747+RNegroni@users.noreply.github.com>
2020-04-24 18:15:29 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -56,3 +56,50 @@ plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 par(mfrow=c(2,1))
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+
+
+#comparison with previous time 
+
+setwd("
+load("rs.RData")
+ls()
+
+library(raster)
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+plot (p224r63_1988)
+
+#exercise: plot in visible RGB 
+par(nfor=c(2,1))
+#plotRGB
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR
+
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+#Exercise in false colour
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+#enhance the noise
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+
+dvi2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre
+cl <- colorRampPalette(c("darkorchid3",'light blue','lightpink4'))(100) 
+plot(dvi2011)
+
+dvi1998 <- p224r63_1998$B4_sre - p224r63_1998$B3_sre
+cl <- colorRampPalette(c("darkorchid3",'light blue','lightpink4'))(100) 
+plot(dvi1998)
+
+    diff <- dvi2011 - dvi1988
+ 
+
+    p224r63_2011res <- aggregate(p224r63_2011, fact=10)
+ 
+
+    p224r63_2011res100 <- aggregate(p224r63_2011, fact=100)
+ 
